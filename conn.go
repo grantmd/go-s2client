d7e5ea2c2e36d60787f577b63ef03e8db6423807
Myles Grant <myles@mylesgrant.com>
2017-08-25 18:30:25 -0700

    Be explicit, I guess

--- conn.go
+++ conn.go
@@ -14,20 +14,20 @@ func (c *Conn) Dial(addr *string) (err error) {
 	u := url.URL{Scheme: "ws", Host: *addr, Path: "/sc2api"}
 	c.ws, _, err = websocket.DefaultDialer.Dial(u.String(), nil)
 	defer c.ws.Close()
-	return
+	return err
 }
 
 func (c *Conn) Read() (message []byte, err error) {
 	_, message, err = c.ws.ReadMessage()
-	return
+	return message, err
 }
 
 func (c *Conn) Write(data []byte) (err error) {
 	err = c.ws.WriteMessage(websocket.TextMessage, data)
-	return
+	return err
 }
 
 func (c *Conn) Close() (err error) {
 	err = c.ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-	return
+	return err
 }
