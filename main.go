3e4ae582d016afe855e21ea50964c4bb9866dae6
Myles Grant <myles@mylesgrant.com>
2017-08-26 14:31:38 -0700

    Working, if noisy and unproductive, game loop

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"github.com/golang/protobuf/proto"
 	"github.com/grantmd/go-s2client/sc2proto"
 	"log"
+	"time"
 )
 
 var addr = flag.String("addr", "localhost:5000", "sc2api server address")
@@ -28,6 +30,7 @@ func main() {
 
 	var req *SC2APIProtocol.Request
 
+	// Create a new game
 	ourPlayer := &SC2APIProtocol.PlayerSetup{
 		Type:       SC2APIProtocol.PlayerType_Participant.Enum(),
 		Race:       SC2APIProtocol.Race_Terran.Enum(),
@@ -45,9 +48,108 @@ func main() {
 					BattlenetMapName: "Antiga Shipyard",
 				},
 				PlayerSetup: []*SC2APIProtocol.PlayerSetup{ourPlayer, opponentPlayer},
+				DisableFog:  proto.Bool(false),
+				Realtime:    proto.Bool(true),
 			},
 		},
 	}
+	err = protocol.SendRequest(req)
+	if err != nil {
+		log.Fatal("Could not send request:", err)
+	}
+
+	_, err = protocol.ReadResponse()
+	if err != nil {
+		log.Fatal("Could not receive response:", err)
+	}
+
+	// Join the game
+	req = &SC2APIProtocol.Request{
+		Request: &SC2APIProtocol.Request_JoinGame{
+			JoinGame: &SC2APIProtocol.RequestJoinGame{
+				Participation: &SC2APIProtocol.RequestJoinGame_Race{
+					Race: SC2APIProtocol.Race_Terran,
+				},
+				Options: &SC2APIProtocol.InterfaceOptions{
+					Raw: proto.Bool(true),
+				},
+			},
+		},
+	}
+	err = protocol.SendRequest(req)
+	if err != nil {
+		log.Fatal("Could not send request:", err)
+	}
+
+	_, err = protocol.ReadResponse()
+	if err != nil {
+		log.Fatal("Could not receive response:", err)
+	}
+
+	// Game loop
+	for {
+		// Request observation
+		req = &SC2APIProtocol.Request{
+			Request: &SC2APIProtocol.Request_Observation{
+				Observation: &SC2APIProtocol.RequestObservation{},
+			},
+		}
+		err = protocol.SendRequest(req)
+		if err != nil {
+			log.Fatal("Could not send request:", err)
+		}
+
+		// Read the game state result
+		resp, err := protocol.ReadResponse()
+		if err != nil {
+			log.Fatal("Could not receive response:", err)
+		}
+
+		obs := resp.GetObservation()
+
+		// Are we done?
+		if resp.GetStatus() == SC2APIProtocol.Status_ended {
+			break
+		}
+
+		// Examine game state
+		for _, unit := range obs.Observation.RawData.Units {
+			if *unit.UnitType == 18 { // Command center
+
+			}
+		}
+
+		// Send action
+
+		// Keep this reasonably paced
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	// Leave game
+	req = &SC2APIProtocol.Request{
+		Request: &SC2APIProtocol.Request_LeaveGame{
+			LeaveGame: &SC2APIProtocol.RequestLeaveGame{},
+		},
+	}
+	err = protocol.SendRequest(req)
+	if err != nil {
+		log.Fatal("Could not send request:", err)
+	}
+
+	_, err = protocol.ReadResponse()
+	if err != nil {
+		log.Fatal("Could not receive response:", err)
+	}
+
+	// Disconnect
+	err = protocol.Disconnect()
+	if err != nil {
+		log.Fatal("Error disconnecting:", err)
+	}
+
+	log.Println("exiting")
+
+	// Extra requests I've tested
 	/*req = &SC2APIProtocol.Request{
 		Request: &SC2APIProtocol.Request_AvailableMaps{
 			AvailableMaps: &SC2APIProtocol.RequestAvailableMaps{},
@@ -63,20 +165,4 @@ func main() {
 			Debug: &SC2APIProtocol.RequestDebug{},
 		},
 	}*/
-	err = protocol.SendRequest(req)
-	if err != nil {
-		log.Fatal("Could not send request:", err)
-	}
-
-	_, err = protocol.ReadResponse()
-	if err != nil {
-		log.Fatal("Could not receive response:", err)
-	}
-
-	err = protocol.Disconnect()
-	if err != nil {
-		log.Fatal("Error disconnecting:", err)
-	}
-
-	log.Println("exiting")
 }
