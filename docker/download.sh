ebb225379b695843be7231c0244c73a442e32f7e
Myles Grant <myles@mylesgrant.com>
2020-01-01 22:11:11 -0800

    Add the new versions and other new downloads

--- docker/download.sh
+++ docker/download.sh
@@ -16,6 +16,30 @@ pushd 4.1.2
 wget --continue http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.1.2.60604_2018_05_16.zip
 popd
 
+pushd 4.6
+wget --continue http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.6.0.67926.zip
+popd
+
+pushd 4.6.1
+wget --continue http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.6.1.68195.zip
+popd
+
+pushd 4.6.2
+wget --continue http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.6.2.69232.zip
+popd
+
+pushd 4.7
+wget --continue http://blzdistsc2-a.akamaihd.net/Linux/SC2.AStar.4.7.zip
+popd
+
+pushd 4.7.1
+wget --continue http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.7.1.zip
+popd
+
+pushd 4.10
+wget --continue http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.10.zip
+popd
+
 mkdir -p downloads/
 
 pushd downloads
@@ -26,6 +50,11 @@ wget --continue http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2017Season3_Upda
 wget --continue http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2017Season4.zip
 wget --continue http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2018Season1.zip
 wget --continue http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2018Season2_Updated.zip
+wget --continue http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2018Season3.zip
+wget --continue http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2018Season4.zip
+wget --continue http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2019Season1.zip
+wget --continue http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2019Season2.zip
+wget --continue http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2019Season3.zip
 wget --continue http://blzdistsc2-a.akamaihd.net/MapPacks/Melee.zip
 
 wget --continue http://blzdistsc2-a.akamaihd.net/ReplayPacks/3.16.1-Pack_1-fix.zip
