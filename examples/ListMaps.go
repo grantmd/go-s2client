78a709417ab501e5abee5fea62b5c46528112ecb
Myles Grant <myles@mylesgrant.com>
2020-01-07 21:03:00 -0800

    Begin consolidation (not intended design yet)

--- examples/ListMaps.go
+++ examples/ListMaps.go
@@ -4,33 +4,17 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
-	"sort"
-	"syscall"
 
 	"github.com/grantmd/go-s2client"
-	"github.com/grantmd/go-s2client/sc2proto"
 )
 
 var gamePort = flag.Int("GamePort", 5000, "Ladder server port")
 var gameServer = flag.String("GameServer", "localhost", "Address of server")
 
-var quitRequested bool
-var isMultiplayer bool
-
-var abilities []*SC2APIProtocol.AbilityData // Most Useful: AbilityId, Available, FootprintRadius
-var units []*SC2APIProtocol.UnitTypeData    // Most Useful: UnitId, Available, MineralCost, VespeneCost, FoodRequired
-var upgrades []*SC2APIProtocol.UpgradeData  // Most Useful: UpgradeId, MineralCost, VespeneCost
-var buffs []*SC2APIProtocol.BuffData        // Most Useful: BuffId
-
-var mapUnits [][]uint32
-
 func main() {
 	// Parse command line args and configure logging
 	flag.Parse()
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
-	isMultiplayer = true
 
 	// Connect to game server
 	addr := fmt.Sprintf("%s:%d", *gameServer, *gamePort)
@@ -48,48 +32,7 @@ func main() {
 	}
 	defer protocol.Disconnect()
 
-	// Setup signal handling
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		<-sigs
-		log.Println("Quit requested")
-		protocol.Disconnect()
-		os.Exit(1)
-	}()
-
-	// Start sending commands/reading responses
-	var req *SC2APIProtocol.Request
-	var resp *SC2APIProtocol.Response
-
-	req = &SC2APIProtocol.Request{
-		Request: &SC2APIProtocol.Request_AvailableMaps{
-			AvailableMaps: &SC2APIProtocol.RequestAvailableMaps{},
-		},
-	}
-	log.Println("Listing available maps…")
-	err = protocol.SendRequest(req)
-	if err != nil {
-		log.Fatal("Could not send available maps request:", err)
-	}
-
-	resp, err = protocol.ReadResponse()
-	if err != nil {
-		log.Fatal("Could not receive available maps response:", err)
-	}
-
-	availableMaps := resp.GetAvailableMaps()
-	sort.Strings(availableMaps.LocalMapPaths)
-	fmt.Println("Local maps:")
-	for _, localMap := range availableMaps.LocalMapPaths {
-		fmt.Println(localMap)
-	}
-
-	sort.Strings(availableMaps.BattlenetMapNames)
-	fmt.Println("\nBattlenet maps:")
-	for _, bnetMap := range availableMaps.BattlenetMapNames {
-		fmt.Println(bnetMap)
-	}
+	s2client.ListMaps(protocol)
 
 	// Disconnect
 	log.Println("Disconnecting…")
---
+++ game.go
@@ -0,0 +1,122 @@
+package s2client
+
+import (
+	"fmt"
+	"log"
+	"sort"
+
+	"github.com/golang/protobuf/proto"
+	SC2APIProtocol "github.com/grantmd/go-s2client/sc2proto"
+)
+
+func ListMaps(sc2 *Protocol) {
+	// Start sending commands/reading responses
+	var req *SC2APIProtocol.Request
+	var resp *SC2APIProtocol.Response
+
+	req = &SC2APIProtocol.Request{
+		Request: &SC2APIProtocol.Request_AvailableMaps{
+			AvailableMaps: &SC2APIProtocol.RequestAvailableMaps{},
+		},
+	}
+	log.Println("Listing available maps…")
+	err := sc2.SendRequest(req)
+	if err != nil {
+		log.Fatal("Could not send available maps request:", err)
+	}
+
+	resp, err = sc2.ReadResponse()
+	if err != nil {
+		log.Fatal("Could not receive available maps response:", err)
+	}
+
+	availableMaps := resp.GetAvailableMaps()
+	sort.Strings(availableMaps.LocalMapPaths)
+	fmt.Println("Local maps:")
+	for _, localMap := range availableMaps.LocalMapPaths {
+		fmt.Println(localMap)
+	}
+
+	sort.Strings(availableMaps.BattlenetMapNames)
+	fmt.Println("\nBattlenet maps:")
+	for _, bnetMap := range availableMaps.BattlenetMapNames {
+		fmt.Println(bnetMap)
+	}
+}
+
+func CreateGame(sc2 Protocol, mapName string, mapPath string) {
+
+	var req *SC2APIProtocol.Request
+	var resp *SC2APIProtocol.Response
+
+	if mapName != "" || mapPath != "" {
+		// Create a new game
+		ourPlayer := &SC2APIProtocol.PlayerSetup{
+			Type: SC2APIProtocol.PlayerType_Participant.Enum(),
+			Race: SC2APIProtocol.Race_Terran.Enum(),
+		}
+		opponentPlayer := &SC2APIProtocol.PlayerSetup{
+			Type:       SC2APIProtocol.PlayerType_Computer.Enum(),
+			Race:       SC2APIProtocol.Race_Terran.Enum(),
+			Difficulty: SC2APIProtocol.Difficulty_VeryHard.Enum(),
+		}
+
+		// Set either a battlenet map or a local map from args
+		if mapName != "" {
+			req = &SC2APIProtocol.Request{
+				Request: &SC2APIProtocol.Request_CreateGame{
+					CreateGame: &SC2APIProtocol.RequestCreateGame{
+						Map: &SC2APIProtocol.RequestCreateGame_BattlenetMapName{
+							BattlenetMapName: mapName,
+						},
+						PlayerSetup: []*SC2APIProtocol.PlayerSetup{ourPlayer, opponentPlayer},
+						DisableFog:  proto.Bool(false),
+						Realtime:    proto.Bool(false),
+					},
+				},
+			}
+		}
+		if mapPath != "" {
+			req = &SC2APIProtocol.Request{
+				Request: &SC2APIProtocol.Request_CreateGame{
+					CreateGame: &SC2APIProtocol.RequestCreateGame{
+						Map: &SC2APIProtocol.RequestCreateGame_LocalMap{
+							LocalMap: &SC2APIProtocol.LocalMap{
+								MapPath: &mapPath,
+							},
+						},
+						PlayerSetup: []*SC2APIProtocol.PlayerSetup{ourPlayer, opponentPlayer},
+						DisableFog:  proto.Bool(false),
+						Realtime:    proto.Bool(false),
+					},
+				},
+			}
+		}
+
+		log.Println("Starting game…")
+		err := sc2.SendRequest(req)
+		if err != nil {
+			log.Fatal("Could not send game start request:", err)
+		}
+		log.Println("Request sent")
+
+		resp, err = sc2.ReadResponse()
+		if err != nil {
+			log.Fatal("Could not receive game start response:", err)
+		}
+		log.Println("Game started:", resp)
+		// TODO: Handle this: "Game started: create_game:<error:InvalidMapPath error_details:"map_path '/SC2/StarCraftII/maps/CollectMineralsAndGas.SC2Map' file doesn't exist." > status:launched"
+	}
+}
+
+// List of unit/ability/upgrade/buff types:
+// https://github.com/Blizzard/s2client-api/blob/master/include/sc2api/sc2_typeenums.h
+
+// Best Scores:
+// MoveToBeacon: 27
+// CollectMineralShards: 109
+// CollectMineralsAndGas: 5986
+// BuildMarines: 30
+// DefeatZerglingsAndBanelings: 67
+// DefeatRoaches: 46
+// FindAndDefeatZerglings: 10
